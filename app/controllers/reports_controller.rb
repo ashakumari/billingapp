2acfe1be67198d37fa852c69a254d9b03e7054d0
ashakumari <ashakumari@gmail.com>
2020-05-25 19:22:43 +0530

    Update reports_controller.rb

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -24,11 +24,11 @@ class ReportsController < ApplicationController
 		filter_criteria_18_gst_cash = "bills.payment_mode = 'Cash' AND bill_items.gst_rate = 18" + " AND " + date_filter_criteria
 		filter_criteria_18_gst_credit = "bills.payment_mode = 'Bank' AND bill_items.gst_rate = 18" + " AND " + date_filter_criteria
 
-		@bills_28_percent_gst_cash_payment = Bill.joins(:bill_items).where(filter_criteria_28_gst_cash).select("bills.id, bills.*, sum(bill_items.amount) / 1.28 as total_taxable_amount").group("bill_id")
-		@bills_28_percent_gst_credit_payment = Bill.joins(:bill_items).where(filter_criteria_28_gst_credit).select("bills.id, bills.*, sum(bill_items.amount) / 1.28 as total_taxable_amount").group("bill_id")
+		@bills_28_percent_gst_cash_payment = Bill.joins(:bill_items).where(filter_criteria_28_gst_cash).select("bills.*, sum(bill_items.amount) / 1.28 as total_taxable_amount").group("bills.id")
+		@bills_28_percent_gst_credit_payment = Bill.joins(:bill_items).where(filter_criteria_28_gst_credit).select("bills.*, sum(bill_items.amount) / 1.28 as total_taxable_amount").group("bills.id")
 
-		@bills_18_percent_gst_cash_payment = Bill.joins(:bill_items).where(filter_criteria_18_gst_cash).select("bills.id, bills.*, sum(bill_items.amount) / 1.18 as total_taxable_amount").group("bill_id")
-		@bills_18_percent_gst_credit_payment = Bill.joins(:bill_items).where(filter_criteria_18_gst_credit).select("bills.id, bills.*, sum(bill_items.amount) / 1.18 as total_taxable_amount").group("bill_id")
+		@bills_18_percent_gst_cash_payment = Bill.joins(:bill_items).where(filter_criteria_18_gst_cash).select("bills.*, sum(bill_items.amount) / 1.18 as total_taxable_amount").group("bills.id")
+		@bills_18_percent_gst_credit_payment = Bill.joins(:bill_items).where(filter_criteria_18_gst_credit).select("bills.*, sum(bill_items.amount) / 1.18 as total_taxable_amount").group("bills.id")
   	
   	end
 end
